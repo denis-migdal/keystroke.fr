220be4c093c58e0dfa732cf1cbe509c06b6844d8
Denis MIGDAL <denis@migdal.ovh>
2021-04-01 09:23:16 +0200

    Add html-minifier + dev/prod targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,92 @@
+const fs = require('fs');
+
+const website_builder = require('./src/main.js');
+
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-var path = require('path');
-
-let website = require('./src/main.js');
 
 let {build_website} = require('prehtml-loader/webpack_helper.js');
 
-let html_config = (dst_path, src) => {
 
-	return {
-		module: {
-			rules: [{
-				enforce: 'post',
-				test: /\.html$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						outputPath: './../', // required
-						name: dst_path
+
+module.exports = (_, {mode}) => {
+
+
+	let is_production = mode === 'production';
+
+	let path_suffix = is_production ? 'prod' : 'dev';
+
+	let website = website_builder(is_production, path_suffix);
+
+	if( fs.existsSync(`${__dirname}/dist/${path_suffix}`) )
+		fs.rmdirSync(`${__dirname}/dist/${path_suffix}`, { recursive: true });
+
+	if( fs.existsSync(`${__dirname}/out`) )
+		fs.rmdirSync(`${__dirname}/out`, { recursive: true });
+
+	let html_config = (dst_path, src) => {
+
+		return {
+			module: {
+				rules: [{
+					enforce: 'post',
+					test: /\.html$/,
+					use: {
+						loader: 'file-loader',
+						options: { name: dst_path }
 					}
+				},{
+					test: /\.html$/,
+					use: is_production ? ['html-minifier-loader', 'prehtml-loader'] : ['prehtml-loader'],
 				}
-			},{
-				test: /\.html$/,
-				use: ['prehtml-loader'],
+			]},
+			entry: { 
+				main: src
+			},
+			output: {
+				path: __dirname,
+				publicPath: '',
+				filename: `./out/${dst_path}.junk`
 			}
-		]},
-		entry: { 
-			main: src
-		},
-		output: {
-			path: path.resolve(__dirname, 'out'),
-			publicPath: '',
-			filename: `${dst_path}.junk`,
-			//clean: true
-		}
+		};
 	};
-};
 
-let js_config = (dst_path, src) => {
+	let js_config = (dst_path, src) => {
 
-	return {
-		module: {
-			rules: [{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			}, {
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				loader: 'url-loader'
+		return {
+			module: {
+				rules: [{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}, {
+					test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+					loader: 'url-loader'
+				}
+				]
+			},
+			entry: {
+				main: src
+			},
+			output: {
+				path: __dirname,
+				publicPath: '',
+				filename: `${dst_path}`,
 			}
-			]
-		},
-		entry: {
-			main: src
-		},
-		output: {
-			path: path.resolve(__dirname),
-			publicPath: '',
-			filename: dst_path,
-			//clean: true
-		}
+		};
 	};
+
+
+	let config = build_website(website, html_config, js_config);
+
+	let lconfig = config[config.length-1];
+	lconfig.plugins = lconfig.plugins ?? [];
+
+	lconfig.plugins.push( new CopyWebpackPlugin({patterns: [
+													{ from: 'assets', to: `dist/${path_suffix}/__assets` },
+													{ from: 'src/template/img', to: `dist/${path_suffix}/__templates/__default/img` }
+												]} ));
+
+
+
+	return config;
+
 };
-
-
-let config = build_website(website, html_config, js_config);
-
-let lconfig = config[config.length-1];
-lconfig.plugins = lconfig.plugins ?? [];
-
-lconfig.plugins.push( new CopyWebpackPlugin({patterns: [
-												{ from: 'assets', to: 'dist/__assets' },
-												{ from: 'src/template/img', to: 'dist/__templates/__default/img' }
-											]} ));
-
-module.exports = config;
