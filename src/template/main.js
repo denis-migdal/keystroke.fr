fc1fc92e0e03b703db10e1cfa64d6e5f40790d06
Denis MIGDAL <denis@migdal.ovh>
2021-05-06 20:28:07 +0200

    Remove bootstrap JS

--- src/template/main.js
+++ src/template/main.js
@@ -1,6 +1,20 @@
-import 'bootstrap';
+//import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import './sticky-footer-navbar.css';
 //import 'webpack-icons-installer/bootstrap'; // https://www.npmjs.com/package/webpack-icons-installer
 
 import './main.css';
+
+
+const $ = require('./jquery_base.js');
+
+$( () => {
+
+	$('.dropdown-toggle').on('click', (ev) => {
+
+		ev.preventDefault();
+
+		let menu = $(ev.currentTarget).parent().find('.dropdown-menu');
+		menu.toggleClass('show');
+	});
+});
